b033994561d7aafcc52822aa382f3a39e5cbc687
Aniket Gadre <aniket.g2185@gmail.com>
2021-10-01 19:05:05 +0800

    fix preprocessor

--- src/parse/preprocessor.rs
+++ src/parse/preprocessor.rs
@@ -6,15 +6,14 @@ use crate::model::Request;
 
 pub fn process(request: Request, env_map: &HashMap<String, String>) -> Request {
     if env_map.len() > 0 {
-        let mut s_request = serde_json::to_string(&request).expect("Request cannot be serialized");
+        let mut s_request = serde_yaml::to_string(&request).expect("Request cannot be serialized");
         s_request = param_substitution(s_request, &env_map);
-        match serde_json::from_str(&s_request) {
+        return match serde_yaml::from_str::<Request>(&s_request) {
             Ok(r) => r,
             Err(err) => {
                 error!("Unable to deserialize request object after parameter replacement. Returning original request");
                 error!("String: {}, Error: {}", s_request, err);
-                return request
-            
+                request
             }
         }
     }
